21b33daf8d4431638c9e9cab819ec417d0efdca0
Crystal <inkloud3@gmail.com>
2022-08-04 14:33:37 +0200

    xx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,9 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
-        if (env('APP_ENV') === 'production') {
-            URL::forceSchema('https');
-        }
+        //
     }
 }
