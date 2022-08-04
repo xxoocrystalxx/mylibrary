803b48739790a848c3e675f8990dd0ac6aa3335f
Crystal <inkloud3@gmail.com>
2022-08-04 22:30:50 +0200

    uibuild

--- app/Http/Controllers/Home/HomeSliderController.php
+++ app/Http/Controllers/Home/HomeSliderController.php
@@ -19,29 +19,55 @@ class HomeSliderController extends Controller
     public function UpdateSlider(Request $request)
     {
         $slide_id = $request->id;
-        if ($request->file('home_slide')) {
-            $image = $request->file('home_slide');
-            error_log($image);
-            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
-            Image::make($image)->resize(636, 852)->save('upload/home_slide/' . $name_gen);
-            $save_url = 'upload/home_slide/' . $name_gen;
-            HomeSlide::findorFail($slide_id)->update([
-                'title' => $request->title,
-                'short_title' => $request->short_title,
-                'video_url' => $request->video_url,
-                'home_slide' => $save_url
+        if ($slide_id) {
+            if ($request->file('home_slide')) {
+                $image = $request->file('home_slide');
+                error_log($image);
+                $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+                Image::make($image)->resize(636, 852)->save('upload/home_slide/' . $name_gen);
+                $save_url = 'upload/home_slide/' . $name_gen;
+                HomeSlide::findorFail($slide_id)->update([
+                    'title' => $request->title,
+                    'short_title' => $request->short_title,
+                    'video_url' => $request->video_url,
+                    'home_slide' => $save_url
 
-            ]);
-            $notification = array('message' => 'Home Slide Updated Successfully', 'alert-type' => 'success');
+                ]);
+                $notification = array('message' => 'Home Slide Updated Successfully', 'alert-type' => 'success');
+            } else {
+                HomeSlide::findorFail($slide_id)->update([
+                    'title' => $request->title,
+                    'short_title' => $request->short_title,
+                    'video_url' => $request->video_url,
+
+                ]);
+                $notification = array('message' => 'Home Slide Updated without Image Successfully', 'alert-type' => 'success');
+            } //end Else
         } else {
-            HomeSlide::findorFail($slide_id)->update([
-                'title' => $request->title,
-                'short_title' => $request->short_title,
-                'video_url' => $request->video_url,
 
-            ]);
-            $notification = array('message' => 'Home Slide Updated without Image Successfully', 'alert-type' => 'success');
-        } //end Else
+            if ($request->file('home_slide')) {
+                $image = $request->file('home_slide');
+                error_log($image);
+                $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+                Image::make($image)->resize(636, 852)->save('upload/home_slide/' . $name_gen);
+                $save_url = 'upload/home_slide/' . $name_gen;
+                HomeSlide::insert([
+                    'title' => $request->title,
+                    'short_title' => $request->short_title,
+                    'video_url' => $request->video_url,
+                    'home_slide' => $save_url
+                ]);
+                $notification = array('message' => 'Home Slide Updated Successfully', 'alert-type' => 'success');
+            } else {
+                HomeSlide::insert([
+                    'title' => $request->title,
+                    'short_title' => $request->short_title,
+                    'video_url' => $request->video_url,
+                ]);
+                $notification = array('message' => 'Home Slide Updated without Image Successfully', 'alert-type' => 'success');
+            }
+        }
+
         return redirect()->back()->with($notification);
     } // End Method
 
