18f205aae892b02f3d0b5c51d53b50188cda5e4e
Crystal <inkloud3@gmail.com>
2023-01-24 19:44:38 +0100

    uibuild

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,13 +3,13 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
+     * 
      * @return void
      */
     public function register()
@@ -19,11 +19,14 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
-     *
+     *@param UrlGenerator $url
      * @return void
      */
-    public function boot()
+
+    public function boot(UrlGenerator $url)
     {
-        // 
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
