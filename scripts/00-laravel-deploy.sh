d78c5b8c961de96423fcf55027181a5bffaf245d
Crystal <inkloud3@gmail.com>
2023-01-26 21:54:36 +0100

    uibuild

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
@@ -9,5 +8,5 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
+#echo "Running migrations..."
+#php artisan migrate --force
